645f652ca0739062ea3b2aa299b24996164d5526
Michael Kolesidis <michael.kolesidis@gmail.com>
2021-10-28 14:14:31 +0300

    Update insertion-sort.js

--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -1,6 +1,6 @@
 /* Insertion Sort 
  *
- * The functions runs four times, each time sorting
+ * The function runs four times, each time sorting
  * an array of n size. It outputs the time needed
  * to sort the array each time, illustrating the 
  * fact that when n increases by 4, time increases 
